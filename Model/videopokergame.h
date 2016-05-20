1f31837b4258143f69beda5c95236aabf4384875
michael landry <michael landry>
2016-05-20 09:26:36 -0300

    added comment

--- Model/videopokergame.h
+++ Model/videopokergame.h
@@ -1,3 +1,19 @@
+///////////////////////////////////////////////////////////////////////
+// File:  Jack or Bette Games
+//
+// Author: Michael Landry
+// This assignment represents my own work and is in accordance with the College Academic Policy
+//
+// Copyright (c) 2016 All Right Reserved by Michael Landry
+// Contributors:
+// Description:  This is the game of poker Jack or Better, this programme simulate 5 card poker hand
+// and the player is able to change any card for a redraw and have new card. the player win if I have a pair of jack or better
+//
+// Date: mai 13 2016
+// Revisions:
+//
+/////////////////////////////////////////////////////////////////////
+
 #ifndef VIDEOPOKERGAME_H
 #define VIDEOPOKERGAME_H
 
